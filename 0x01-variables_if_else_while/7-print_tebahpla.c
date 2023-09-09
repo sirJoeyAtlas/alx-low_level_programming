4df853fa389ace1032f6c761a8b6684d65af4933
sirJoeyAtlas <ndougundo770@gmail.com>
2023-09-08 17:55:22 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - smile in the mirror
+ * Return: 0
+ */
+int main(void)
+{
+char letter;
+
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar(letter);
+putchar('\n');
+return (0);
+}
