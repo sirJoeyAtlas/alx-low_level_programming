5eb10d64cec70521ca5a66d35b3ae4639c3357b5
sirJoeyAtlas <ndougundo770@gmail.com>
2023-09-12 15:19:05 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet x10
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+char c;
+int i = 0;
+while (i <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+i++;
+}
+}
