57baa319e753f2dc470cd79c71d0aa74cff21097
sirJoeyAtlas <ndougundo770@gmail.com>
2023-09-21 15:44:02 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - The main function
+ * @str: function parameter of the string
+ * Return: void
+ */
+char *cap_string(char *str)
+{
+int count = 0;
+
+while (str[count])
+{
+while (!(str[count] >= 'a' && str[count] <= 'z'))
+count++;
+if (str[count - 1] == ' ' ||
+str[count - 1] == '\t' ||
+str[count - 1] == '\n' ||
+str[count - 1] == ',' ||
+str[count - 1] == ';' ||
+str[count - 1] == '.' ||
+str[count - 1] == '!' ||
+str[count - 1] == '?' ||
+str[count - 1] == '"' ||
+str[count - 1] == '(' ||
+str[count - 1] == ')' ||
+str[count - 1] == '{' ||
+str[count - 1] == '}' ||
+count == 0)
+str[count] -= 32;
+count++;
+}
+return (str);
+}
