9bbeafa19a488cd6e1a4b352e40285bae3c67dec
sirJoeyAtlas <ndougundo770@gmail.com>
2023-10-19 19:01:30 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+void HundredFirstbyGundo(void)__attribute__ ((constructor));
+/**
+ * HundredFirst - main function
+ */
+void HundredFirstbyGundo(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
