8b1bc2f317158514e2ffc010e9559e6782beccb2
sirJoeyAtlas <ndougundo770@gmail.com>
2023-10-19 19:03:57 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 void HundredFirstbyGundo(void)__attribute__ ((constructor));
 /**
- * HundredFirst - main function
+ * HundredFirst: main function
  */
 void HundredFirstbyGundo(void)
 {
